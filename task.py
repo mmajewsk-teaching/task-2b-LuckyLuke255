8d031a07a1e6b01c26672a2cbc88afb62308775f
LuckyLuke255 <105583948+LuckyLuke255@users.noreply.github.com>
2025-11-17 14:44:48 +0100

    task.py

--- task.py
+++ task.py
@@ -1,30 +1,19 @@
-# Class diary
-#
-# Create program for handling lesson scores.
-# Use python to handle student (highscool) class scores, and attendance.
-# Make it possible to:
-# - Get students total average score (average across classes)
-# - get students average score in class
-# - hold students name and surname
-# - Count total attendance of student
-#
-# Please, use your imagination and create more functionalities.
-# Your project should be able to handle entire school(s?).
-# If you have enough courage and time, try storing (reading/writing)
-# data in text files (YAML, JSON).
-# If you have even more courage, try implementing user interface (might be text-like).
-#
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#Your program must be runnable with command "python task.py".
-#Show some usecases of your library in the code (print some things)
-#
-#When you are done upload this code to your github repository. 
-#
-#Delete these comments before commit!
-#Good luck.
+class Diary():
+    def __init__(self):
+        self.students = []
+    
+    def add_student(self, name: str, grade: double):
+        for i in enumerate(self.students):
+            if student[0] == name:
+                self.student(i) == (name, grade)
+                return self.students.append((name, grade))
+
+    def get_grade(self name:str, name: double):
+        for student in self.students:
+            if student[0] == name:
+                return student[1]
+        return None
+
+    
+def avg_grade(self.students):
+    return sum(self.students) / len(self.students)
